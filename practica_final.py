64783ee682638dd36218fd6bfd4523d0dd199b99
Ana-MM <74472452+Ana-MM@users.noreply.github.com>
2023-07-20 06:02:55 +0200

    practicaFinal

---
+++ practica_final.py
@@ -0,0 +1,65 @@
+import random 
+
+# Validar los campos
+def validar_campos(prompt):
+    while True:
+        valor = input(prompt)
+        try:
+            numero_entero = int(valor)
+            if numero_entero > 0:
+                return numero_entero
+            else:
+                print("Error: Ingresa un número entero mayor que 0.")
+        except ValueError:
+            print("Error: Ingresa solo números enteros.")
+
+size = validar_campos("Introduce el tamaño de la matriz: ")
+
+#Creamos la matriz
+def crear_matriz(size):
+    matriz = []
+    suma_filas = []
+    suma_columnas = []
+    
+    for f in range(size):
+        fila = []
+        for c in range(size):
+            n_random = random.randint(0, 9)
+            
+            if len(suma_filas)-1 < f: suma_filas.append(n_random)
+            else: suma_filas[f] = suma_filas[f] + n_random
+            
+            if f == 0: suma_columnas.append(n_random)
+            else: suma_columnas[c] = suma_columnas[c] + n_random
+            
+            fila.append(n_random)
+        matriz.append(fila)
+        
+    return {
+        "matriz": matriz, 
+        "suma_filas": suma_filas,
+        "suma_columnas": suma_columnas
+    }
+
+resultado = crear_matriz(size)
+print ("Matriz de numeros:", resultado["matriz"])
+
+
+# # Sumar cada fila
+# sumas_filas = []
+# for fila in matriz:
+#     suma_fila = sum(fila)
+#     sumas_filas.append(suma_fila)
+
+# # Sumar cada columna
+# sumas_columnas = []
+# for c in range(columns):
+#     suma_columna = sum(fila[c] for fila in matriz)
+#     sumas_columnas.append(suma_columna)
+
+# Imprimir las sumas de cada fila
+print("Sumas de cada fila:", resultado["suma_filas"])
+
+
+# Imprimir las sumas de cada columna
+print("Sumas de cada columna:", resultado["suma_columnas"])
